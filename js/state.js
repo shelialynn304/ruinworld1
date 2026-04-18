2a0909f626af2ca9cd2f5cf8298e5dc39227c344
shelialynn304 <shelialynn304@gmail.com>
2026-04-17 23:06:20 -0400

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,19 @@
+window.gameState = {
+  playerName: "Wanderer",
+  currentScene: "intro",
+  corruption: 0,
+  mercy: 0,
+  influence: 0,
+  devotion: 0,
+  followerCount: 0,
+  gold: 0,
+  relics: [],
+  flags: {
+    prayedAtRoad: false,
+    foundBoneCharm: false,
+    tookRoadCoins: false,
+    threatenedMara: false,
+    trustedMara: false,
+    insultedMara: false
+  }
+};
