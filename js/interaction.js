f327929778f3dbfcb260a2f3d2bb08ea39123499
shelialynn304 <shelialynn304@gmail.com>
2026-04-30 17:56:02 -0400

    Add zoomed player-follow camera transform

--- js/interaction.js
+++ js/interaction.js
@@ -1,18 +1,13 @@
 import { circleNearRect } from "./collision.js";
 import { consumeInteractPress } from "./input.js";
 import { openDialogue, isDialogueOpen } from "./dialogue.js";
+import { getPlayerFeetPosition } from "./player.js";
 
 export function getNearbyInteractable(player, map) {
-  const center = {
-    x: player.x + player.width / 2,
-    y: player.y + player.height / 2
-  };
+  const feet = getPlayerFeetPosition(player);
 
   return map.interactables.find((interactable) =>
-    circleNearRect(
-      { x: center.x, y: center.y, radius: interactable.radius },
-      interactable
-    )
+    circleNearRect({ x: feet.x, y: feet.y, radius: interactable.radius }, interactable)
   );
 }
 
